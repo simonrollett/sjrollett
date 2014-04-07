77ddc65fa561fbc13548f88e3d3518ceae194d46
rollett <srollett@qmetric.co.uk>
2014-04-07 08:27:48 +0100

    heading block styles

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
     <?php include "includes/header.php";?>
 
-        <section class="content-1 col-12 pad mgn-S">
+        <section class="content-1 col-12">
 
-            <h1 class="img-block">
-                <a class="img-block-link" href="">
-                    Simon Rollett:
-                    <span class="img-block-link-caption">
-                        Managing Digital Media
+            <h1 class="heading-block">
+                <a class="heading-block-link" href="">
+                    Digital Media:
+                    <span class="heading-block-link-caption">
+                        Making Media Deliver
                     </span>
                 </a>
             </h1>
-            <h2 class="img-block">
-                <a class="img-block-link" href="">
+            <h2 class="heading-block">
+                <a class="heading-block-link" href="">
                     My Future:
-                    <span class="img-block-link-caption">
+                    <span class="heading-block-link-caption">
                         What I'm looking for
                     </span>
                 </a>
             </h2>
-            <h2 class="img-block">
-                <a class="img-block-link" href="">
+            <h2 class="heading-block">
+                <a class="heading-block-link" href="">
                     History:
-                    <span class="img-block-link-caption">
+                    <span class="heading-block-link-caption">
                         Story so far
                     </span>
                 </a>
